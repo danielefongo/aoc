5d547d64cb029e3c2b34376de52dcd857b1b9a80
danielefongo <danielefongo@gmail.com>
2022-12-06 08:48:43 +0100

    day6: part2

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,20 +1,25 @@
+use std::{collections::HashSet, hash::Hash};
+
 use crate::utils::read_input;
 
 pub fn run() {
     let input = read_input(6);
 
-    let marker = (0..input.len())
-        .find(|&i| all_unique(input[i..(i + 4)].chars().collect::<Vec<char>>()))
-        .map(|i| i + 4)
-        .unwrap();
-
-    println!("Part1: {}", marker);
+    println!("Part1: {}", find_marker(&input, 4));
+    println!("Part2: {}", find_marker(&input, 14));
 }
 
-fn all_unique<T: Ord + Clone>(data: Vec<T>) -> bool {
-    let mut data2 = data.clone();
-    data2.sort();
-    data2.dedup();
+fn find_marker(input: &String, distinct_chars: usize) -> usize {
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(distinct_chars)
+        .enumerate()
+        .find(|(_, window)| all_unique(window))
+        .map(|(i, _)| i + distinct_chars)
+        .unwrap()
+}
 
-    data2.len() == data.len()
+fn all_unique<T: Ord + Clone + Hash>(data: &[T]) -> bool {
+    data.iter().cloned().collect::<HashSet<T>>().len() == data.len()
 }
