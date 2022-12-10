418591b9a4028e71dc51ee6d0cbaaa66db1aa077
danielefongo <danielefongo@gmail.com>
2022-12-10 14:38:49 +0100

    day10: part2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,61 +1,157 @@
+use std::ops::Deref;
+
 use crate::utils::{lines, read_input};
 
 #[derive(Debug)]
-enum Op {
+enum MemoryOp {
     Add(i32),
     Noop,
 }
-impl From<String> for Op {
+impl From<String> for MemoryOp {
     fn from(input: String) -> Self {
         if input == "noop" {
-            Op::Noop
+            MemoryOp::Noop
         } else if input.starts_with("addx") {
             let (_, value) = input.split_once(" ").unwrap();
-            Op::Add(value.parse().unwrap())
+            MemoryOp::Add(value.parse().unwrap())
         } else {
             panic!("invalid input")
         }
     }
 }
 
+#[derive(Debug, Clone)]
+enum CpuOp {
+    Add(i32),
+    Loading,
+    Noop,
+}
+
+#[derive(Debug, Clone)]
+struct CpuOps {
+    ops: Vec<CpuOp>,
+}
+impl From<Vec<MemoryOp>> for CpuOps {
+    fn from(ops: Vec<MemoryOp>) -> Self {
+        let ops: Vec<CpuOp> = ops
+            .iter()
+            .map(|it| match it {
+                MemoryOp::Add(val) => vec![CpuOp::Loading, CpuOp::Add(val.clone())],
+                MemoryOp::Noop => vec![CpuOp::Noop],
+            })
+            .flatten()
+            .collect();
+        Self { ops }
+    }
+}
+impl Deref for CpuOps {
+    type Target = Vec<CpuOp>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.ops
+    }
+}
+
 #[derive(Debug)]
 struct Register {
-    history: Vec<i32>,
+    actual: i32,
+    step: usize,
+    ops: CpuOps,
 }
 impl Register {
-    fn new() -> Self {
-        Self { history: vec![1] }
-    }
-    fn run(&mut self, op: Op) {
-        let last_value = self.history.last().unwrap_or(&1).clone();
-
-        match op {
-            Op::Add(value) => {
-                self.history.push(last_value);
-                self.history.push(last_value + value)
-            }
-            Op::Noop => {
-                self.history.push(last_value);
-            }
+    fn new(ops: CpuOps) -> Self {
+        Self {
+            actual: 1,
+            step: 0,
+            ops,
         }
     }
-    fn value_during(&self, step_number: usize) -> i32 {
-        self.history.get(step_number - 1).unwrap().clone()
+    fn execute(&mut self, op: CpuOp) {
+        if let CpuOp::Add(val) = op {
+            self.actual += val;
+        }
+        self.step += 1;
+    }
+}
+impl Iterator for Register {
+    type Item = (usize, i32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.step == 0 {
+            self.execute(CpuOp::Noop);
+        } else {
+            let op = self.ops.get(self.step - 1)?;
+            self.execute(op.clone());
+        }
+        Some((self.step, self.actual))
+    }
+}
+
+struct CRT {
+    register: Register,
+    lines: Vec<String>,
+}
+impl CRT {
+    fn new(ops: CpuOps) -> Self {
+        Self {
+            register: Register::new(ops),
+            lines: Vec::new(),
+        }
+    }
+    fn get_line(&mut self, line: usize) -> Option<&mut String> {
+        while self.lines.get(line).is_none() {
+            self.lines
+                .push("........................................".to_owned());
+        }
+
+        self.lines.get_mut(line)
+    }
+    fn print_on_line(&mut self, line: usize, pixel: usize) {
+        self.get_line(line)
+            .unwrap()
+            .replace_range(pixel..(pixel + 1), "#");
+    }
+    fn run(&mut self) {
+        while let Some((step, register_value)) = self.register.next() {
+            let step = step as i32;
+            let line_idx = (step - 1) / 40;
+            let pixel_idx = (step - 1) % 40;
+            let (pixel_min, pixel_max) = (register_value - 1, register_value + 1);
+
+            if pixel_idx >= pixel_min && pixel_idx <= pixel_max {
+                self.print_on_line(line_idx as usize, pixel_idx as usize)
+            }
+        }
     }
 }
 
 pub fn run() {
-    let mut register = Register::new();
-
-    lines(read_input(10))
+    let ops: CpuOps = lines(read_input(10))
         .into_iter()
-        .map(Op::from)
-        .for_each(|a| register.run(a));
+        .map(MemoryOp::from)
+        .collect::<Vec<MemoryOp>>()
+        .into();
 
-    let values = [20, 60, 100, 140, 180, 220]
-        .into_iter()
-        .map(|it| register.value_during(it as usize) * it)
-        .sum::<i32>();
+    part1(ops.clone());
+    part2(ops);
+}
 
-    println!("Part1: {}", values);
+fn part1(ops: CpuOps) {
+    let register = Register::new(ops);
+
+    println!(
+        "Part1: {:?}",
+        register
+            .skip(19)
+            .step_by(40)
+            .map(|(idx, value)| value * (idx as i32))
+            .sum::<i32>()
+    );
+}
+
+fn part2(ops: CpuOps) {
+    let mut crt = CRT::new(ops);
+    crt.run();
+    println!("Part2:");
+    crt.lines.iter().for_each(|it| println!("{}", it));
 }
