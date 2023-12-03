149e7d35fca1d55659b31ce5281a2d0890e7f1ed
danielefongo <danielefongo@gmail.com>
2023-12-03 12:13:52 +0100

    feat(2023): day01, part2

--- year_2023/src/day01/mod.rs
+++ year_2023/src/day01/mod.rs
@@ -1,12 +1,16 @@
+use std::convert::identity;
+
 use utils::{lines, read_input};
 
 pub fn run() {
-    println!("Part1: {}", part1(lines(read_input!())));
+    println!("Part1: {}", run_part(lines(read_input!()), identity));
+    println!("Part2: {}", run_part(lines(read_input!()), replace_digits));
 }
 
-fn part1(input: Vec<String>) -> u32 {
+fn run_part(input: Vec<String>, mapper: impl Fn(String) -> String) -> u32 {
     input
         .into_iter()
+        .map(mapper)
         .map(|line| {
             let first = line.chars().filter(char::is_ascii_digit).take(1);
             let second = line.chars().filter(char::is_ascii_digit).rev().take(1);
@@ -14,4 +18,16 @@ fn part1(input: Vec<String>) -> u32 {
         })
         .map(|numbers_line| numbers_line.parse::<u32>().unwrap_or_default())
         .sum()
+}
+
+fn replace_digits(line: String) -> String {
+    line.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
 }
