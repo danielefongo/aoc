526a5681dc9e5652bc9f0d121ed1bb8ff7e48d1c
danielefongo <danielefongo@gmail.com>
2023-12-04 08:40:00 +0100

    feat(2023): day04, part2

--- year_2023/src/day04/mod.rs
+++ year_2023/src/day04/mod.rs
@@ -1,26 +1,30 @@
-use utils::{extract, lines, read_input};
+use utils::{extract, extract_one, lines, read_input};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Card {
+    id: usize,
     winning_numbers: Vec<u32>,
     numbers: Vec<u32>,
 }
 impl Card {
     fn points(&self) -> u32 {
-        let matching_numbers = self
-            .numbers
-            .iter()
-            .filter(|n| self.winning_numbers.contains(n))
-            .count() as u32;
+        let matching_numbers = self.won_cards();
         if matching_numbers > 0 {
             2_i32.pow(matching_numbers - 1) as u32
         } else {
             0
         }
     }
+    fn won_cards(&self) -> u32 {
+        self.numbers
+            .iter()
+            .filter(|n| self.winning_numbers.contains(n))
+            .count() as u32
+    }
 }
 impl From<String> for Card {
     fn from(value: String) -> Self {
+        let id = extract_one(&value, "\\d+").parse().unwrap();
         let value = value.split(':').collect::<Vec<_>>()[1];
         let inputs = value.split('|').collect::<Vec<_>>();
         let winning_numbers = extract(inputs[0], "\\d+")
@@ -33,6 +37,7 @@ impl From<String> for Card {
             .collect();
 
         Self {
+            id,
             winning_numbers,
             numbers,
         }
@@ -40,12 +45,28 @@ impl From<String> for Card {
 }
 
 pub fn run() {
-    println!(
-        "Part1: {}",
-        lines(read_input!())
-            .into_iter()
-            .map(Card::from)
-            .map(|it| it.points())
-            .sum::<u32>()
-    );
+    let cards = lines(read_input!())
+        .into_iter()
+        .map(Card::from)
+        .collect::<Vec<_>>();
+
+    println!("Part1: {}", part1(&cards));
+    println!("Part2: {}", part2(&cards));
+}
+
+fn part1(cards: &[Card]) -> u32 {
+    cards.iter().map(|it| it.points()).sum::<u32>()
+}
+fn part2(cards: &[Card]) -> u32 {
+    let mut count = 0;
+    let mut stack = cards.iter().map(Clone::clone).collect::<Vec<_>>();
+    while let Some(actual_card) = stack.pop() {
+        count += 1;
+        cards
+            .iter()
+            .skip(actual_card.id)
+            .take(actual_card.won_cards() as usize)
+            .for_each(|c| stack.push(c.clone()));
+    }
+    count
 }
