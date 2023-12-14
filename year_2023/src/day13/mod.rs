6eaa759694e37e2ee88b5393967dcaeee9bc22d5
danielefongo <danielefongo@gmail.com>
2023-12-14 17:33:32 +0100

    feat(2023): day13, part2

--- year_2023/src/day13/mod.rs
+++ year_2023/src/day13/mod.rs
@@ -1,3 +1,5 @@
+use std::{iter::Zip, vec::IntoIter};
+
 use utils::{lines, read_input};
 
 fn transpose(strings: &[String]) -> Vec<String> {
@@ -27,30 +29,48 @@ impl MirrorPos {
     }
 }
 
+enum Rule {
+    NoSmudges,
+    Smudges,
+}
+impl Rule {
+    fn compare(&self, first: u32, second: u32) -> bool {
+        match self {
+            Self::NoSmudges => first == second,
+            Self::Smudges => (first ^ second).count_ones() <= 1,
+        }
+    }
+    fn valid_mirror(&self, zip: Zip<IntoIter<&u32>, IntoIter<&u32>>) -> bool {
+        match self {
+            Rule::NoSmudges => zip.clone().all(|(a, b)| a == b),
+            Rule::Smudges => zip.map(|(a, b)| (a ^ b).count_ones()).sum::<u32>() == 1,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Mirrors {
     vertical: Vec<u32>,
     horizontal: Vec<u32>,
 }
 impl Mirrors {
-    fn find_mirror(&self) -> MirrorPos {
-        if let Some(pos) = self.find_mirror_pos(&self.vertical) {
+    fn find_mirror(&self, rule: Rule) -> MirrorPos {
+        if let Some(pos) = self.find_mirror_pos(&self.vertical, &rule) {
             return MirrorPos::Vertical(pos + 1);
         }
-        if let Some(pos) = self.find_mirror_pos(&self.horizontal) {
+        if let Some(pos) = self.find_mirror_pos(&self.horizontal, &rule) {
             return MirrorPos::Horizontal(pos + 1);
         }
+
         MirrorPos::None
     }
-    fn find_mirror_pos(&self, lines: &Vec<u32>) -> Option<usize> {
+    fn find_mirror_pos(&self, lines: &Vec<u32>, rule: &Rule) -> Option<usize> {
         (0..(lines.len() - 1))
-            .filter(|it| lines[*it] == lines[it + 1])
+            .filter(|it| rule.compare(lines[*it], lines[it + 1]))
             .find(|pos| {
                 let left = lines.iter().take(pos + 1).rev().collect::<Vec<_>>();
                 let right = lines.iter().skip(pos + 1).collect::<Vec<_>>();
-                let all_eq = left.iter().zip(right.iter()).all(|(a, b)| a == b);
-
-                all_eq
+                rule.valid_mirror(left.into_iter().zip(right))
             })
     }
 }
@@ -72,11 +92,22 @@ impl From<Vec<String>> for Mirrors {
 }
 
 pub fn run() {
-    let elements = read_input!()
-        .split("\n\n")
-        .map(|block| Mirrors::from(lines(block.to_string())))
-        .map(|mirrors| mirrors.find_mirror())
-        .map(MirrorPos::score)
-        .sum::<usize>();
-    println!("Part1: {:?}", &elements);
+    println!(
+        "Part1: {:?}",
+        read_input!()
+            .split("\n\n")
+            .map(|block| Mirrors::from(lines(block.to_string())))
+            .map(|mirrors| mirrors.find_mirror(Rule::NoSmudges))
+            .map(MirrorPos::score)
+            .sum::<usize>()
+    );
+    println!(
+        "Part2: {:?}",
+        read_input!()
+            .split("\n\n")
+            .map(|block| Mirrors::from(lines(block.to_string())))
+            .map(|mirrors| mirrors.find_mirror(Rule::Smudges))
+            .map(MirrorPos::score)
+            .sum::<usize>()
+    );
 }
